0e6ed6289652c1d49ed4018927fed1d5909f9820
rainedragon-dev <cityzen.dale.ward@proton.me>
2024-11-20 14:07:02 +0000

    Code improvements

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 def enter_three_sentances():
 
-    # Takes inut form user t oset three sentances
-    sentance_one = input("Enter sentance 1: ")
-    sentance_two = input("Enter sentance 2: ")
-    sentance_three = input("Enter sentance 3: ")
+    # Takes input form user for three sentances
+    sentence_one = input("Enter sentence 1: ")
+    sentence_two = input("Enter sentence 2: ")
+    sentence_three = input("Enter sentence 3: ")
     splitter = "-----------"
 
     # Opens user_sentance.txt file for writing
     f = open('user_sentances.txt', 'w')
 
 
-    # Writes list to open file and closes file
-    f.write(f"{sentance_one}\n")
-    f.write(f"{splitter}\n")
-    f.write(f"{sentance_two}\n")
-    f.write(f"{splitter}\n")
-    f.write(f"{sentance_three}")
-    f.close()
+    # Writes list to open file
+    with open('user_sentences.txt', 'w') as file:
+        file.write(f"{sentence_one}\n")
+        file.write(f"{splitter}\n")
+        file.write(f"{sentence_two}\n")
+        file.write(f"{splitter}\n")
+        file.write(f"{sentence_three}")
+
     print("Sentances have been saved to user_sentances.txt.")
 
 enter_three_sentances()
